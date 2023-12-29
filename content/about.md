90dea2c18a5cdfa0d78aea82380d9dca98d9623c
he.wenyao <he.wenyao@nint.com>
2023-12-29 16:34:05 +0800

    first commit

---
+++ content/about.md
@@ -0,0 +1,18 @@
+# About Content v2
+
+```js
+import colors from "tailwindcss/colors";
+import typography from "@tailwindcss/typography";
+
+export default {
+  darkMode: "class",
+  plugins: [typography()],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+};
+
+```
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "nuxt-icon",
+  ],
+  // https://color-mode.nuxtjs.org
+  colorMode: {
+    classSuffix: "",
+  },
+  // https://content.nuxtjs.org
+  content: {
+    documentDriven: true,
+    highlight: {
+      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: {
+        dark: "github-dark",
+        default: "github-light",
+      },
+    },
+  },
+});
