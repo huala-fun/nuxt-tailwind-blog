12724882e4bba1124dd3497ffa53b65ccff1ccf0
= <huala.fun@outlook.com>
2024-01-04 23:52:54 +0800

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  tailwindcss: {
-    cssPath: "~/main.css",
-  },
   modules: [
     "nuxt-content-assets",
     "@nuxt/content",
