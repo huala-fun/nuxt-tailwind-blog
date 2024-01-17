dcd7750ea8eb7e3a63712599acf9e6d1a3934dc6
= <huala.fun@outlook.com>
2024-01-17 23:50:57 +0800

    fix

--- components/ColorModeSwitch.vue
+++ components/ColorModeSwitch.vue
@@ -7,11 +7,11 @@ const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'da
   <button aria-label="Color Mode" class="inline-block w-5" @click="onClick">
     <ColorScheme placeholder="...">
       <template v-if="colorMode.value === 'dark'">
-        <Icon name="dark-mode" class="w-5 h-5" />
+        <IconsMaterialSymbolsDarkModeOutline name="dark-mode" class="w-5 h-5" />
         <span class="sr-only">Dark Mode</span>
       </template>
       <template v-else>
-        <Icon name="light-mode" class="w-5 h-5" />
+        <IconsMaterialSymbolsLightModeOutline name="light-mode" class="w-5 h-5" />
         <span class="sr-only">Light Mode</span>
       </template>
     </ColorScheme>
